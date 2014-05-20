1a6f3b2b2fa618d046ae279b37b228377347e6cc
Bert Frees <bert.frees@sbs.ch>
2014-05-20 14:20:59 +0200

    Change XProcSpecRunner API
    Instead of returning all results at the end, provide a `Reporter'
    interface with callbacks for notifications and for returning
    intermediate results after each test. The default Reporter prints a text
    based report to stdout. Deprecates the `TestLogger' interface.
    
    The Reporter interface could be improved over time to allow for more
    advanced reports. Currently the test results are much less detailed in
    Java than in HTML. This is because each test is run externally with
    XProcSpec and the results are obtained by parsing the Surefire
    reports. These reports are currently not expressive enough; a lot of the
    metadata and structure of the original test format is lost.
    
    One idea to improve on this is to parse the "internal" XProcSpec report
    format. To make this more robust the internal format should be made more
    official.
    
    Another idea to improve the integration is to capture XProcSpec's log
    messages and make them available in Java through callbacks. A possible
    solution would be to override `xproc/utils/log.xpl` with a custom
    Calabash step.
    
    An implementation of XProcSpec in Java would eliminate the need for
    parsing XML reports completely, and it would also make the logging
    integration easier.

--- xproc-maven-plugin/src/main/java/org/daisy/maven/xproc/plugin/XProcSpecMojo.java
+++ xproc-maven-plugin/src/main/java/org/daisy/maven/xproc/plugin/XProcSpecMojo.java
@@ -1,16 +1,12 @@
 package org.daisy.maven.xproc.plugin;
 
 import java.io.File;
-import java.util.ArrayList;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.MojoFailureException;
-import org.codehaus.plexus.util.DirectoryScanner;
 
 import org.daisy.maven.xproc.xprocspec.XProcSpecRunner;
-import org.daisy.maven.xproc.xprocspec.XProcSpecRunner.TestLogger;
-import org.daisy.maven.xproc.xprocspec.XProcSpecRunner.TestResult;
+import org.daisy.maven.xproc.xprocspec.XProcSpecRunner.Reporter;
 
 /**
  * Run an XProcSpec test.
@@ -54,64 +50,15 @@ public class XProcSpecMojo extends AbstractMojo {
 	
 	public void execute() throws MojoFailureException {
 		
-		final Log logger = getLog();
-		
-		TestLogger testLogger = new TestLogger() {
-			public void info(String message) { logger.info(message); }
-			public void warn(String message) { logger.warn(message); }
-			public void error(String message) { logger.error(message); }
-			public void debug(String message) { logger.debug(message); }
-		};
-		
-		logger.info("------------------------------------------------------------------------");
-		logger.info("XPROCSPEC TESTS");
-		logger.info("------------------------------------------------------------------------");
-		
 		XProcSpecRunner runner = new XProcSpecRunner();
+		Reporter.DefaultReporter reporter = new Reporter.DefaultReporter(System.out);
 		
-		TestResult[] testResults = runner.run(xprocspecDirectory,
-		                                      reportsDirectory,
-		                                      surefireReportsDirectory,
-		                                      tempDir,
-		                                      testLogger);
-		
-		int run = testResults.length;
-		ArrayList<String> failures = new ArrayList<String>();
-		ArrayList<String> errors = new ArrayList<String>();
-		ArrayList<String> skipped = new ArrayList<String>();
-		
-		for (TestResult result : testResults) {
-			switch (result.state) {
-			case FAILURE:
-				failures.add(result.name);
-				break;
-			case ERROR:
-				errors.add(result.name);
-				break;
-			case SKIPPED:
-				skipped.add(result.name);
-				break;
-			}
-		}
-		
-		logger.info("");
-		logger.info("Summary:");
-		logger.info("--------");
-		logger.info("");
-		if (failures.size() > 0) {
-			logger.info("Failed tests:");
-			for (String s : failures)
-				logger.info("\t" + s);
-			logger.info(""); }
-		if (errors.size() > 0) {
-			logger.info("Tests in error:");
-			for (String s : errors)
-				logger.info("\t" + s);
-			logger.info(""); }
-		logger.info("Tests run: " + run + ", Failures: " + failures.size() + ", Errors: "+ errors.size());
-		logger.info("");
-		if (failures.size() + errors.size() > 0)
-			throw new MojoFailureException("Some tests failed or had errors.");
+		if (!runner.run(xprocspecDirectory,
+		                reportsDirectory,
+		                surefireReportsDirectory,
+		                tempDir,
+		                reporter))
+			throw new MojoFailureException("There are test failures.");
 		
 	}
 }
