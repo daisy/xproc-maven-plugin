1d5e2ac310dec0eb9ea030bca6dec34ecd8e19f5
Bert Frees <bertfrees@gmail.com>
2016-06-08 17:22:26 +0200

    Support %indent{} in encoder pattern

--- xproc-engine-daisy-pipeline-logging/src/main/java/org/daisy/maven/xproc/pipeline/logging/ProgressMessageEncoder.java
+++ xproc-engine-daisy-pipeline-logging/src/main/java/org/daisy/maven/xproc/pipeline/logging/ProgressMessageEncoder.java
@@ -9,6 +9,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.pattern.ClassicConverter;
 import ch.qos.logback.core.pattern.PatternLayoutEncoderBase;
 
+import org.daisy.pipeline.client.models.JobMessages;
 import org.daisy.pipeline.client.models.Message;
 
 public class ProgressMessageEncoder extends PatternLayoutEncoderBase<ILoggingEvent> {
@@ -26,6 +27,7 @@ public class ProgressMessageEncoder extends PatternLayoutEncoderBase<ILoggingEve
 			public Map<String,String> getEffectiveConverterMap() {
 				Map<String,String> map = new HashMap<String,String>(super.getEffectiveConverterMap());
 				map.put("progress", ProgressConverter.class.getName());
+				map.put("indent", IndentConverter.class.getName());
 				map.put("m", MessageConverter.class.getName());
 				map.put("msg", MessageConverter.class.getName());
 				map.put("message", MessageConverter.class.getName());
@@ -42,9 +44,8 @@ public class ProgressMessageEncoder extends PatternLayoutEncoderBase<ILoggingEve
 	
 	@Override
 	public void doEncode(ILoggingEvent event) throws IOException {
+		Message m = addMessageFromLogbackMessage(event);
 		if (skipIfOnlyProgress) {
-			Message m = new Message();
-			m.text = event.getFormattedMessage();
 			if (m.getText().isEmpty())
 				return; }
 		super.doEncode(event);
@@ -52,18 +53,56 @@ public class ProgressMessageEncoder extends PatternLayoutEncoderBase<ILoggingEve
 	
 	public static class MessageConverter extends ClassicConverter {
 		public String convert(ILoggingEvent event) {
-			Message m = new Message();
-			m.text = event.getFormattedMessage();
-			return m.getText();
+			return currentMessage().getText();
 		}
 	}
 	
 	public static class ProgressConverter extends ClassicConverter {
 		public String convert(ILoggingEvent event) {
-			Message m = new Message();
-			m.text = event.getFormattedMessage();
-			return m.getProgressInfo();
+			return currentMessage().getProgressInfo();
 		}
 	}
 	
+	public static class IndentConverter extends ClassicConverter {
+		
+		private String childIndent;
+		
+		@Override
+		public void start() {
+			childIndent = getFirstOption();
+			super.start();
+		}
+		
+		public String convert(ILoggingEvent event) {
+			return indent(currentMessage().depth);
+		}
+		
+		private String indent(int depth) {
+			if (depth > 0) {
+				StringBuilder indent = new StringBuilder();
+				for (int i = 0; i < depth; i++)
+					indent.append(childIndent);
+				return indent.toString(); }
+			else
+				return "";
+		}
+	}
+	
+	private static Message currentMessage() {
+		return messages.get(sequence - 1);
+	}
+	
+	// threat everything as one single job
+	private static final JobMessages messages = new JobMessages();
+	
+	private static int sequence = 0;
+	
+	private static Message addMessageFromLogbackMessage(ILoggingEvent event) {
+		Message m = new Message();
+		m.text = event.getFormattedMessage();
+		m.sequence = sequence++;
+		messages.add(m);
+		// use get() in order to force computation of depth
+		return messages.get(sequence - 1);
+	}
 }
